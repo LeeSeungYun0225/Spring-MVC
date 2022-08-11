bafd9018dd51ed3bc1fcade5d22ad083c8fa712e
Seung Yun LEE <soul3914@naver.com>
2022-08-12 03:42:16 +0900

    Spring Annotation

--- src/main/java/com/newlecture/web/constants/Dbconstants.java
+++ src/main/java/com/newlecture/web/constants/Dbconstants.java
@@ -2,14 +2,33 @@ package com.newlecture.web.constants;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Dbconstants {
 
+	
 	public static DataSource datasource;
 
+
+	
+	public Dbconstants() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	// static 변수는 setter에 autowired하지 않으면 nullpointer예외 발생 
+	@Autowired
 	public void setDatasource(DataSource datasource) {
 		this.datasource = datasource;
 		
 	}
+
+	
 	
 	
 }
